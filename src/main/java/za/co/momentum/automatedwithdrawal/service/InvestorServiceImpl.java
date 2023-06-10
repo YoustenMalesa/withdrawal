898c47d550b7570b9a648fc86d8e2fc05bf18083
F5395038 <F5395038@FNB.CO.ZA>
2023-06-10 23:07:41 +0200

    remove mock

--- src/main/java/za/co/momentum/automatedwithdrawal/service/InvestorServiceImpl.java
+++ src/main/java/za/co/momentum/automatedwithdrawal/service/InvestorServiceImpl.java
@@ -2,15 +2,8 @@ package za.co.momentum.automatedwithdrawal.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import za.co.momentum.automatedwithdrawal.model.Address;
-import za.co.momentum.automatedwithdrawal.model.Contact;
 import za.co.momentum.automatedwithdrawal.model.Investor;
-import za.co.momentum.automatedwithdrawal.model.Product;
 import za.co.momentum.automatedwithdrawal.repository.InvestorRepository;
-
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 @Service
 public class InvestorServiceImpl implements InvestorService {
