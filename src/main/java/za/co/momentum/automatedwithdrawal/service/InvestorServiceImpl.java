83eaa625b58686133244f3c2c3afc51eeff5d86c
F5395038 <F5395038@FNB.CO.ZA>
2023-06-10 23:07:15 +0200

    remove mock

--- src/main/java/za/co/momentum/automatedwithdrawal/service/InvestorServiceImpl.java
+++ src/main/java/za/co/momentum/automatedwithdrawal/service/InvestorServiceImpl.java
@@ -23,50 +23,11 @@ public class InvestorServiceImpl implements InvestorService {
 
     @Override
     public Investor findById(Long id) {
-        //mock();
         return investorRepository.findById(id).orElse(null);
     }
 
     @Override
     public void updateInvestor(Investor investor) {
         investorRepository.saveAndFlush(investor);
-    }
-
-    void mock() {
-        Investor investor = new Investor();
-        investor.setFirstName("Yousten");
-        investor.setLastName("Malesa");
-        investor.setDateOfBirth(LocalDate.of(1992, 8, 7));
-
-        Address address = new Address();
-        address.setCity("Midrand");
-        address.setSuburb("Clayville");
-        address.setStreetDetails("4093 Krypton street");
-
-        Contact contact = new Contact();
-        contact.setEmail("youstenmalesa@gmail.com");
-        contact.setMobileNumber("0840351356");
-
-        investor.setAddress(address);
-        investor.setContactDetails(contact);
-
-
-        Product p1 = new Product();
-        p1.setName("Insurance Policy");
-        p1.setType("RETIREMENT");
-        p1.setCurrentBalance(500000D);
-
-        Product p2 = new Product();
-        p2.setName("Investment plus");
-        p2.setType("SAVINGS");
-        p2.setCurrentBalance(250000D);
-
-        Set<Product> products = new HashSet<>();
-        products.add(p1);
-        products.add(p2);
-
-        investor.setProducts(products);
-
-        investorRepository.save(investor);
     }
 }
